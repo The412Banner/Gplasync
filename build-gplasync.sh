25cba7ab4d1f151b679fb9a179e312810d37e185
Ph42oN <julle.ys.57@gmail.com>
2023-05-29 15:30:01 +0300

    build using gitlab ci and improve cache with gpl+async

---
+++ build-gplasync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone --branch v2.2 --recursive https://github.com/doitsujin/dxvk.git
+update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
+update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
+update-alternatives --set i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-posix
+update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix
+cd dxvk
+patch -p1 < ../dxvk-gplasync-2.2-2.patch
+./package-release.sh gplasync-$CI_COMMIT_REF_NAME . --no-package
