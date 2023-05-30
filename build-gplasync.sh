55520219ed8da84ef23db8e7a349a700064f2827
Ph42oN <julle.ys.57@gmail.com>
2023-05-30 14:49:19 +0300

    issue #9 test 3

--- build-gplasync.sh
+++ build-gplasync.sh
@@ -5,6 +5,6 @@ update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++
 update-alternatives --set i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-posix
 update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix
 cd dxvk
-patch -p1 < ../0001-issue-9-test.patch
+patch -p1 < ../dxvk-gplasync-2.1-1.patch
 ./package-release.sh gplasync-$CI_COMMIT_REF_NAME . --no-package
 mv dxvk-gplasync-$CI_COMMIT_REF_NAME ../
