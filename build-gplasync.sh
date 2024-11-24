649caf102bd808479c8c0db03283bceb05a0f75e
Ph42oN <julle.ys.57@gmail.com>
2024-11-24 15:58:11 +0200

    disable memory defrag by default

--- build-gplasync.sh
+++ build-gplasync.sh
@@ -2,7 +2,7 @@
 git clone --branch v2.5.1 --recursive https://github.com/doitsujin/dxvk.git
 
 cd dxvk
-patch -p1 < ../patches/dxvk-gplasync-2.5-1.patch
+patch -p1 < ../patches/dxvk-gplasync-2.5.1-2.patch
 patch -p1 < ../patches/global-dxvk.conf.patch
 ./package-release.sh gplasync-$CI_COMMIT_REF_NAME . --no-package
 mv dxvk-gplasync-$CI_COMMIT_REF_NAME ../
