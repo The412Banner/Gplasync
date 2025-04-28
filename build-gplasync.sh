4b91b345bd54acb0e5ba9d3cad51cf0909545c7f
Ph42oN <julle.ys.57@gmail.com>
2025-04-28 15:34:30 +0300

    update to dxvk 2.6.1

--- build-gplasync.sh
+++ build-gplasync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-git clone --branch v2.6 --recursive https://github.com/doitsujin/dxvk.git
+git clone --branch v2.6.1 --recursive https://github.com/doitsujin/dxvk.git
 
 cd dxvk
-patch -p1 < ../patches/dxvk-gplasync-2.6-1.patch
+patch -p1 < ../patches/dxvk-gplasync-2.6.1-1.patch
 patch -p1 < ../patches/global-dxvk.conf.patch
 ./package-release.sh gplasync-$CI_COMMIT_REF_NAME . --no-package
 mv dxvk-gplasync-$CI_COMMIT_REF_NAME ../
